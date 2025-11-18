f1aae988a09fb41a37b1fc7c5e9fdc8d4505ac53
stephaniee3e <158521537+stephaniee3e@users.noreply.github.com>
2025-11-18 14:11:29 +0000

    Project1

---
+++ main.py
@@ -0,0 +1,8 @@
+first = input ("Please type in the first number:")
+num = int(first)
+second = input ("Please type another number:")
+num = int(second)
+if first > second:
+    print("The greater number was",first)
+else:
+    print("The greater number was",second)
